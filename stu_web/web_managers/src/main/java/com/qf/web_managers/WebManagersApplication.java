efa51309282c640c3b35d8d421bed844061dfece
lds <133003674@qq.com>
2019-11-30 16:59:46 +0800

    第一次提交部署

---
+++ stu_web/web_managers/src/main/java/com/qf/web_managers/WebManagersApplication.java
@@ -0,0 +1,13 @@
+package com.qf.web_managers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebManagersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebManagersApplication.class, args);
+    }
+
+}
---
+++ stu_web/web_managers/src/test/java/com/qf/web_managers/WebManagersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qf.web_managers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebManagersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
